9203327d8e9d20e1eca98e53623a308986afcaec
jogindra kumar <joginder9868@outlook.com>
2022-03-09 18:52:26 +0530

    Update AdminProfileController.php

--- app/Http/Controllers/Backend/AdminProfileController.php
+++ app/Http/Controllers/Backend/AdminProfileController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Admin;
 use Illuminate\Support\Facades\DB;
+use Hash;
 
 class AdminProfileController extends Controller
 {
